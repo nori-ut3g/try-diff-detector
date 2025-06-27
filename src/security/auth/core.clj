4dbff856007f2455db8783073380c7fd8df1c341
nori-ut3g <akinori.nkd@gmail.com>
2025-06-28 00:04:55 +0900

    Update security auth

--- src/security/auth/core.clj
+++ src/security/auth/core.clj
@@ -1,9 +1,10 @@
 (ns security.auth.core)
 
 (defn authenticate [username password]
-  "Authenticate user credentials"
+  "Authenticate user credentials - UPDATED"
   (when (and (seq username) (seq password))
     (println "Authentication attempt for user:" username)
+    (println "New security check added")
     true))
 
 (defn authorize [user role]
